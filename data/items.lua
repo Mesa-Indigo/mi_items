797aeefce5c1ff5fb3b59c47312035a89e83d9a5
Agimir <william@mesaindigostudios.com>
2024-07-11 22:41:39 -0500

    added more items

--- data/items.lua
+++ data/items.lua
@@ -276,7 +276,7 @@ return {
 
     ['phone'] = {
 		label = 'Phone',
-		weight = 190,
+		weight = 500,
 		stack = false,
 		consume = 0,
         description = "Did you miss a call? I\'ll never tell.",
@@ -296,19 +296,12 @@ return {
 	},
 
     ['lockpick'] = {
-        label = 'Lockpick', weight = 160, stack = true,
+        label = 'Lockpick', weight = 80, stack = true,
         description = "When in doubt, steal shit.",
     },
 
-
-
-
-    ----------------------------------------------------------------
-    ----------------------------------------------------------------
-    --[[    Items - Tools    ]]--
-
     ['bandage_basic'] = {
-        label = 'Basic Bandage', weight = 600, stack = true,
+        label = 'Basic Bandage', weight = 1200, stack = true,
         description = "That won't stop a bullet hole but ok I guess.",
     },
 
@@ -331,12 +324,56 @@ return {
         label = 'Police Riot Shield', weight = 4000, stack = false,
         description = "I would be surprised if you got hurt wearing this.",
     },
-    
+
+
+
+
+    ----------------------------------------------------------------
+    ----------------------------------------------------------------
+    --[[    Items - Vehicles    ]]--
+
     ['repair_veheng'] = {
-        label = 'Engine Repair Kit', weight = 7500, stack = false,
-        description = "This will only fix the engine, you goober.",
+        label = 'Engine Repair Kit', weight = 7500, stack = true,
+        description = "I see what's wrong with it. Ain't got no gas in it.",
     },
 
+    ['repair_vehtir'] = {
+        label = 'Tire Repair Kit', weight = 7500, stack = true,
+        description = "Driving with a flat tire? That's a paddlin.",
+    },
+
+    ['repair_vehbod'] = {
+        label = 'Body Repair Kit', weight = 7500, stack = true,
+        description = "Vanity repairs, as that is all that matters.",
+    },
+
+    -- https://wiki.rage.mp/index.php?title=Vehicle_Colors for vehicle color reference
+
+    ['colorveh_red'] = {
+        label = 'Spray Kit: Red', weight = 2000, stack = true,
+        description = "Paints over your car with this color.",
+    },
+
+
+
+    ----------------------------------------------------------------
+    ----------------------------------------------------------------
+    --[[    Items - Chemicals    ]]--
+
+    ['chem_sulfur'] = {
+        label = 'Sulfur', weight = 230, stack = true,
+        description = "Pro tip: Rust is not a vitamin. Also, I need to see a doctor.",
+    },
+
+    ['chem_potnitr'] = {
+        label = 'Potassium Nitrate', weight = 230, stack = true,
+        description = "Pro tip: Rust is not a vitamin. Also, I need to see a doctor.",
+    },
+
+    ['chem_acid'] = {
+        label = 'Sulfuric Acid', weight = 230, stack = true,
+        description = "Pro tip: Rust is not a vitamin. Also, I need to see a doctor.",
+    },
 
 
     ----------------------------------------------------------------
@@ -348,9 +385,29 @@ return {
         description = "Pro tip: Rust is not a vitamin. Also, I need to see a doctor.",
     },
 
+    ['scrap_alluminium'] = {
+        label = 'Alluminium Scrap', weight = 100, stack = true,
+        description = "How do you pronounce this shit?",
+    },
+
+    ['scrap_bolts'] = {
+        label = 'Bolts & Fittings', weight = 100, stack = true,
+        description = "Hahahahaha, you said fittings.",
+    },
+
     ['scrap_wood'] = {
         label = 'Wood Planks', weight = 170, stack = true,
-        description = "words",
+        description = "It is not can you, but 'wood' you pick this up?",
+    },
+
+    ['scrap_plastic'] = {
+        label = 'Plastic Scrap', weight = 100, stack = true,
+        description = "Pro tip: Rust is not a vitamin. Also, I need to see a doctor.",
+    },
+
+    ['scrap_wires'] = {
+        label = 'Bundle of Wires', weight = 100, stack = true,
+        description = "What do you call a wire with a break in it?",
     },
     
 }
