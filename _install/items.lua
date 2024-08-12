559fff78661ee05206b051468ffd788b7da5e865
Agimir <william@mesaindigo.com>
2024-08-11 21:18:29 -0500

    more drugs

--- _install/items.lua
+++ _install/items.lua
@@ -1007,12 +1007,32 @@ return {
 
     ['pd_meth'] = {
         label = 'Powdered Meth', weight = 60, stack = true,
-        description = "Wild cannabis, yet to be processed and vibing in your pocket",
+        description = "If it looks and smells like cocaine, it's probably not cocaine",
     },
 
     ['cr_meth'] = {
         label = 'Crystal Meth', weight = 90, stack = true,
-        description = "Ready to be ground up, sold, or forgotten in your dresser.",
+        description = "Teeth never matter when you can get new ones.",
+    },
+
+    ['bc_meth'] = {
+        label = 'Brick of Crystal Meth', weight = 90, stack = true,
+        description = "The phrase 'hits like a brick' comes to mind, more out there.",
+    },
+
+    ['wd_cocaine'] = {
+        label = 'Coca Leaves', weight = 90, stack = true,
+        description = "Is the one we smoke? No? Imma try it anyways",
+    },
+
+    ['pc_cocaine'] = {
+        label = 'Powdered Cocaine', weight = 90, stack = true,
+        description = "Up the nose as fast as that shit goes",
+    },
+
+    ['bc_cocaine'] = {
+        label = 'Brick of Powdered Cocaine', weight = 90, stack = true,
+        description = "The phrase 'hits like a brick' comes to mind, more out there.",
     },
 
 
