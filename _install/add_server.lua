b741e88549b267c3a054f8843c04f3ea474b2635
Agimir <william@mesaindigo.com>
2024-08-17 23:11:15 -0500

    Delete add_server.lua

--- _install/add_server.lua
+++
@@ -1,7 +0,0 @@
----@diagnostic disable: undefined-global
-
-Item('case_beer_rancho', function(data, slot)
-	ox_inventory:useItem(data, function(data)
-		exports['mi_utils']:case_beer_rancho()
-	end)
-end)
