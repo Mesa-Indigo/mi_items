bb5d117418de8501220f59f2065ff7e0b6829a8b
Agimir <william@mesaindigo.com>
2024-08-19 08:09:41 -0500

    added recipes

--- _install/crafting.lua
+++ _install/crafting.lua
@@ -138,105 +138,260 @@ return {
 	----------------------------------------------------------------
 	{	-- ALCOHOL: General
     items = {
-        {
-            name = 'cariaque_glass', duration = 10000, count = 4,
-            ingredients = {
-                bourgeoix_bottle = 1,
-                alc_cups = 4
-            }
-        },
-        {
-            name = 'bourgeoix_glass', duration = 10000, count = 4,
-            ingredients = {
-                cariaque_bottle = 1,
-                alc_cups = 4
-            }
-        },
-        {
-            name = 'ragga_glass', duration = 10000, count = 4,
-            ingredients = {
-                ragga_bottle = 1,
-                alc_cups = 4
-            }
-        },
-        {
-            name = 'tequilya_glass', duration = 10000, count = 4,
-            ingredients = {
-                tequilya_bottle = 1,
-                alc_cups = 4
-            }
-        },
-        {
-            name = 'nogo_glass', duration = 10000, count = 4,
-            ingredients = {
-                nogo_bottle = 1,
-                alc_cups = 4
-            }
-        },
-        {
-            name = 'mount_glass', duration = 10000, count = 4,
-            ingredients = {
-                mount_bottle = 1,
-                alc_cups = 4
-            }
-        },
-        {
-            name = 'richards_glass', duration = 10000, count = 4,
-            ingredients = {
-                richards_bottle = 1,
-                alc_cups = 4
-            }
-        },
+            {
+                name = 'cariaque_glass', duration = 10000, count = 4,
+                ingredients = {
+                    bourgeoix_bottle = 1,
+                    alc_cups = 4
+                }
+            },
+            {
+                name = 'bourgeoix_glass', duration = 10000, count = 4,
+                ingredients = {
+                    cariaque_bottle = 1,
+                    alc_cups = 4
+                }
+            },
+            {
+                name = 'ragga_glass', duration = 10000, count = 4,
+                ingredients = {
+                    ragga_bottle = 1,
+                    alc_cups = 4
+                }
+            },
+            {
+                name = 'tequilya_glass', duration = 10000, count = 4,
+                ingredients = {
+                    tequilya_bottle = 1,
+                    alc_cups = 4
+                }
+            },
+            {
+                name = 'nogo_glass', duration = 10000, count = 4,
+                ingredients = {
+                    nogo_bottle = 1,
+                    alc_cups = 4
+                }
+            },
+            {
+                name = 'mount_glass', duration = 10000, count = 4,
+                ingredients = {
+                    mount_bottle = 1,
+                    alc_cups = 4
+                }
+            },
+            {
+                name = 'richards_glass', duration = 10000, count = 4,
+                ingredients = {
+                    richards_bottle = 1,
+                    alc_cups = 4
+                }
+            },
 
-        {
-            name = 'cariaque_shot', duration = 10000, count = 8,
-            ingredients = {
-                bourgeoix_bottle = 1,
-                alc_shots = 8
-            }
-        },
-        {
-            name = 'bourgeoix_shot', duration = 10000, count = 8,
-            ingredients = {
-                cariaque_bottle = 1,
-                alc_shots = 8
-            }
-        },
-        {
-            name = 'ragga_shot', duration = 10000, count = 8,
-            ingredients = {
-                ragga_bottle = 1,
-                alc_shots = 8
-            }
-        },
-        {
-            name = 'tequilya_shot', duration = 10000, count = 8,
-            ingredients = {
-                tequilya_bottle = 1,
-                alc_shots = 8
-            }
-        },
-        {
-            name = 'nogo_shot', duration = 10000, count = 8,
-            ingredients = {
-                nogo_bottle = 1,
-                alc_shots = 8
-            }
-        },
-        {
-            name = 'mount_shot', duration = 10000, count = 8,
-            ingredients = {
-                mount_bottle = 1,
-                alc_shots = 8
-            }
-        },
-        {
-            name = 'richards_shot', duration = 10000, count = 8,
-            ingredients = {
-                richards_bottle = 1,
-                alc_shots = 8
-            }
-        },
-    }
-},
+            {
+                name = 'cariaque_shot', duration = 10000, count = 8,
+                ingredients = {
+                    bourgeoix_bottle = 1,
+                    alc_shots = 8
+                }
+            },
+            {
+                name = 'bourgeoix_shot', duration = 10000, count = 8,
+                ingredients = {
+                    cariaque_bottle = 1,
+                    alc_shots = 8
+                }
+            },
+            {
+                name = 'ragga_shot', duration = 10000, count = 8,
+                ingredients = {
+                    ragga_bottle = 1,
+                    alc_shots = 8
+                }
+            },
+            {
+                name = 'tequilya_shot', duration = 10000, count = 8,
+                ingredients = {
+                    tequilya_bottle = 1,
+                    alc_shots = 8
+                }
+            },
+            {
+                name = 'nogo_shot', duration = 10000, count = 8,
+                ingredients = {
+                    nogo_bottle = 1,
+                    alc_shots = 8
+                }
+            },
+            {
+                name = 'mount_shot', duration = 10000, count = 8,
+                ingredients = {
+                    mount_bottle = 1,
+                    alc_shots = 8
+                }
+            },
+            {
+                name = 'richards_shot', duration = 10000, count = 8,
+                ingredients = {
+                    richards_bottle = 1,
+                    alc_shots = 8
+                }
+            },
+        }
+    },
+    ----------------------------------------------------------------
+	----------------------------------------------------------------
+    {	-- WEAPONS: attachments
+        items = {
+            -- tool
+            {
+                name = 'at_flashlight', duration = 5000, count = 3,
+                ingredients = {
+                    scrap_iron = 4,
+                    scrap_bolts = 3,
+                    scrap_bottle = 2,
+                    scrap_plastic = 3
+                }
+            },
+            {
+                name = 'at_grip', duration = 5000, count = 1,
+                ingredients = {
+                    scrap_iron = 4,
+                    scrap_bolts = 3,
+                    scrap_bottle = 2,
+                    scrap_plastic = 3
+                }
+            },
+            -- barrel
+			{
+                name = 'at_barrel', duration = 5000, count = 3,
+                ingredients = {
+                    scrap_iron = 4,
+                    scrap_bolts = 3,
+                    scrap_bottle = 2,
+                    scrap_plastic = 3
+                }
+            },
+            {
+                name = 'at_suppressor_light', duration = 5000, count = 3,
+                ingredients = {
+                    scrap_iron = 4,
+                    scrap_bolts = 3,
+                    scrap_bottle = 2,
+                    scrap_plastic = 3
+                }
+            },
+            {
+                name = 'at_suppressor_heavy', duration = 5000, count = 3,
+                ingredients = {
+                    scrap_iron = 4,
+                    scrap_bolts = 3,
+                    scrap_bottle = 2,
+                    scrap_plastic = 3
+                }
+            },
+            {
+                name = 'at_compensator', duration = 5000, count = 3,
+                ingredients = {
+                    scrap_iron = 4,
+                    scrap_bolts = 3,
+                    scrap_bottle = 2,
+                    scrap_plastic = 3
+                }
+            },
+            -- sights
+			{
+                name = 'at_scope_macro', duration = 5000, count = 3,
+                ingredients = {
+                    scrap_iron = 4,
+                    scrap_bolts = 3,
+                    scrap_bottle = 2,
+                    scrap_plastic = 3
+                }
+            },
+            {
+                name = 'at_scope_small', duration = 5000, count = 3,
+                ingredients = {
+                    scrap_iron = 4,
+                    scrap_bolts = 3,
+                    scrap_bottle = 2,
+                    scrap_plastic = 3
+                }
+            },
+            {
+                name = 'at_scope_medium', duration = 5000, count = 3,
+                ingredients = {
+                    scrap_iron = 4,
+                    scrap_bolts = 3,
+                    scrap_bottle = 2,
+                    scrap_plastic = 3
+                }
+            },
+            {
+                name = 'at_scope_large', duration = 5000, count = 3,
+                ingredients = {
+                    scrap_iron = 4,
+                    scrap_bolts = 3,
+                    scrap_bottle = 2,
+                    scrap_plastic = 3
+                }
+            },
+            -- magazine
+            {
+                name = 'at_clip_extended_pistol', duration = 5000, count = 1,
+                ingredients = {
+                    scrap_iron = 4,
+                    scrap_bolts = 3,
+                    scrap_bottle = 2,
+                    scrap_plastic = 3
+                }
+            },
+			{
+                name = 'at_clip_extended_smg', duration = 5000, count = 3,
+                ingredients = {
+                    scrap_iron = 4,
+                    scrap_bolts = 3,
+                    scrap_bottle = 2,
+                    scrap_plastic = 3
+                }
+            },
+            {
+                name = 'at_clip_extended_shotgun', duration = 5000, count = 1,
+                ingredients = {
+                    scrap_iron = 4,
+                    scrap_bolts = 3,
+                    scrap_bottle = 2,
+                    scrap_plastic = 3
+                }
+            },
+            {
+                name = 'at_clip_extended_rifle', duration = 5000, count = 3,
+                ingredients = {
+                    scrap_iron = 4,
+                    scrap_bolts = 3,
+                    scrap_bottle = 2,
+                    scrap_plastic = 3
+                }
+            },
+            {
+                name = 'at_clip_extended_mg', duration = 5000, count = 1,
+                ingredients = {
+                    scrap_iron = 4,
+                    scrap_bolts = 3,
+                    scrap_bottle = 2,
+                    scrap_plastic = 3
+                }
+            },
+            {
+                name = 'at_clip_extended_sniper', duration = 5000, count = 1,
+                ingredients = {
+                    scrap_iron = 4,
+                    scrap_bolts = 3,
+                    scrap_bottle = 2,
+                    scrap_plastic = 3
+                }
+            },
+            
+        }
+    },
 }
