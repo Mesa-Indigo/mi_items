1a6c6152f92c07d7936ff2b05fbdf88aeb9f837e
Agimir <william@mesaindigo.com>
2024-08-27 08:43:03 -0500

    effect animations

--- client/effects/stamina.lua
+++ client/effects/stamina.lua
@@ -6,7 +6,7 @@ exports('ejunk', function()
     
 end)
 
-RegisterCommand('timer', function()
+local timetest = function()
     local timer = lib.timer(5000, function()
         print("timer ended")
     end, true)
@@ -16,4 +16,21 @@ RegisterCommand('timer', function()
     print(timer:getTimeLeft('m'))  -- 0.08 minutes
     print(timer:getTimeLeft('h'))  -- 0.00 hours
     print(timer:getTimeLeft())     -- {ms = 5000, s = 5.00, m = 0.08, h = 0.00 }
+end
+
+
+RegisterCommand('timer', function()
+    if lib.progressBar({
+        duration = 2000,
+        label = 'Taking Pill',
+        useWhileDead = false,
+        canCancel = true,
+        disable = {
+            car = true,
+        },
+        anim = {
+            dict = 'mp_suicide', clip = 'pill', flag = 49,
+            blendIn = 4.0, blendOut = 4.0, playbackRate = 0
+        },
+    }) then print('Do stuff when complete') else print('Do stuff when cancelled') end
 end, false)
